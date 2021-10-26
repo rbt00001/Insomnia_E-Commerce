26e2281f48aef8bcf301a2189b6dc0380cdaf36f
Ben Townsend <RBT000011@gmail.com>
2021-10-26 17:51:22 -0500

    updated models in index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete:'CASCADE'
+})
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+  onDelete:'CASCADE'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsMany(Tag,{
+   through: "ProductTag",
+   foreignKey: 'product_id'
+   }
+)
 // Tags belongToMany Products (through ProductTag)
-
+Tags.belongsMany(Product, {
+  through: "through ProductTag",
+  foreignKey: "tag_id"
+})
 module.exports = {
   Product,
   Category,
